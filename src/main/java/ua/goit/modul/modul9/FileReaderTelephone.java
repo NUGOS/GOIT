661c50aa38caa5d8aa8b524412fb79647dbf0d27
Dmitriy Lysak <ldv@ldv.com.ua>
2021-06-10 16:30:14 +0300

    add branch dev

--- src/main/java/ua/goit/modul/modul9/FileReaderTelephone.java
+++ src/main/java/ua/goit/modul/modul9/FileReaderTelephone.java
@@ -1,18 +1,40 @@
 package ua.goit.modul.modul9;
 
-import java.io.File;
+import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class FileReaderTelephone {
-    public static void main(String[] args) throws IOException {
-        Path get = Paths.get( "file.txt" );
-        List<String> result = Files.readAllLines(get);
+    private static final String VALIDATE_ONE = "\\d{3}-\\d{3}-\\d{4}";
+    private static final String VALIDATE_TWO = "\\(\\d{3}\\) \\d{3}-\\d{4}";
 
-        System.out.println(result);
+    public static void main(String[] args) throws IOException {
+        InputStream resourceAsStream = FileReaderTelephone.class.getResourceAsStream( "/file.txt" );
+        String result = null;
+        result = read( resourceAsStream, result );
+
+        result.split( "  " );
+        System.out.println( result );
     }
 
+    public static String read(InputStream is, String result) {
+        try (Scanner sc = new Scanner( new BufferedInputStream( is ) )) {
+            StringBuilder sb = new StringBuilder();
+
+            while (sc.hasNext()) {
+                if (!isCorrectlyNumber( result )) {
+                    continue;
+                }
+                sb.append( sc.nextLine() );
+            }
+            return sb.toString();
+
+        }
+    }
+
+    private static boolean isCorrectlyNumber(CharSequence result) {
+        return Pattern.matches( VALIDATE_ONE, result ) || Pattern.matches( VALIDATE_TWO, result );
+    }
 }
