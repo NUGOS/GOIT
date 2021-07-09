deeb45b0610159b1fc73d0113da8fb90b73ec72c
Lysak <ldv@ldv.com.ua>
2021-07-09 16:00:29 +0300

    end Task 4 modul 10

--- src/main/java/ua/goit/modul/modul10/HomeWorkTen.java
+++ src/main/java/ua/goit/modul/modul10/HomeWorkTen.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class HomeWorkTen {
 
@@ -22,6 +23,11 @@ public class HomeWorkTen {
         System.out.print("\u001B[32m ***Task #3*** \u001B[0m");
         System.out.println("\u001B[31m Print ReturnStringSortArray \u001B[0m");
         System.out.println(ReturnStringSortArray(array));
+        System.out.print("\u001B[32m ***Task #4*** \u001B[0m");
+        System.out.println("\u001B[31m Print LongStream (Для теста задан limit) \u001B[0m");
+        System.out.println((LongStream(25214903917L, 11L, (long) Math.pow(2, 48), 44L, 10)).collect(Collectors.toList()));
+
+
     }
 
     /* Task 1 */
@@ -29,7 +35,7 @@ public class HomeWorkTen {
     public static String ReturnStringOddIndex(List<String> nameListString) {
 
         Iterator<String> myIterator = nameListString.stream()
-                                                    .iterator();
+                .iterator();
         StringBuilder result = new StringBuilder("1. " + myIterator.next());
 
         int i = 2;
@@ -51,22 +57,40 @@ public class HomeWorkTen {
 
     public static String ReturnStringReversSort(List<String> nameListString) {
         String resultsort = nameListString.stream()
-                                          .map(String::toUpperCase)
-                                          .sorted(Comparator.reverseOrder())
-                                          .collect(Collectors.joining(", "));
+                .map(String::toUpperCase)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.joining(", "));
         return resultsort;
     }
 
-    /* Task 3 */
+    // Task 3 - тут чет по условию непонятно, что нужно вернуть по тексту похоже на String.
+    // Скоре всего нужно дорабтывать и работать не со стрингой, а с интами и их сортировать,
+    // так как стринг 10 будет после единицы что не есть корректным
 
     public static String ReturnStringSortArray(String[] array) {
         List<String> numbersList = Arrays.asList(array);
         String number = numbersList.stream()
-                                .sorted(Comparator.naturalOrder())
-                                .collect(Collectors.joining(", "));
-        System.out.println(numbersList);
-        return number;
+                .collect(Collectors.joining(", "));
+
+        String[] numberList = number.replaceAll("\\s+", "").split(",");
+        String numberSorted = Arrays.stream(numberList)
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.joining(","));
+        return numberSorted;
     }
 
+    //Task 4
+
+    public static Stream<Long> LongStream(long a, long c, long m, long seed, int limit) {
+        return Stream.iterate(seed, n -> (a * n + c) % m).limit(limit);
+
+
+    }
+
+    //Task 5
+
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+        return null;
+    }
 }
 
--- src/main/java/ua/goit/modul/modul10/ReturnStringSort.java
+++ src/main/java/ua/goit/modul/modul10/ReturnStringSort.java
@@ -1,2 +1,21 @@
-package ua.goit.modul.modul10;public class ReturnStringSort {
+package ua.goit.modul.modul10;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ReturnStringSort {
+    public static void main(String[] args) {
+        List<String> namesList = Arrays.asList("Ivan", "John", "Peter", "Bill");
+        System.out.println(ReturnStringSort(namesList));
+    }
+
+    public static String ReturnStringSort(List<String> namesList) {
+        String resultsort = namesList.stream()
+                .map(String::toUpperCase)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.joining(", "));
+        return resultsort;
+    }
 }
