2361409a130db5a2c0bf17f1bdd7540afe5b3d9d
Dmitriy Lysak <ldv@ldv.com.ua>
2021-07-30 08:13:09 +0300

    refactor modul 10 task 5

--- src/main/java/ua/goit/modul/modul10/HomeWorkTen.java
+++ src/main/java/ua/goit/modul/modul10/HomeWorkTen.java
@@ -1,6 +1,9 @@
 package ua.goit.modul.modul10;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -25,8 +28,8 @@ public class HomeWorkTen {
         System.out.println((LongStream(25214903917L, 11L, (long) Math.pow(2, 48), 44L, 10)).collect(Collectors.toList()));
         System.out.print("\u001B[32m ***Task #5*** \u001B[0m");
         System.out.println("\u001B[31m Print zip Stream \u001B[0m");
-        Stream<Integer> first = Stream.of(1, 3, 5, 7, 9);
-        Stream<Integer> second = Stream.of(2, 4, 6, 8);
+        Stream<String> first = Stream.of("A", "B", "C");
+        Stream<String> second = Stream.of("1", "2", "3", "4");
         zip(first, second).forEach(System.out::print);
 
     }
@@ -57,27 +60,21 @@ public class HomeWorkTen {
     /* Task 2 */
 
     public static String ReturnStringReversSort(List<String> nameListString) {
-        String resultsort = nameListString.stream()
-                                          .map(String::toUpperCase)
-                                          .sorted(Comparator.reverseOrder())
-                                          .collect(Collectors.joining(", "));
-        return resultsort;
+        return nameListString.stream()
+                             .map(String::toUpperCase)
+                             .sorted(Comparator.reverseOrder())
+                             .collect(Collectors.joining(", "));
     }
 
     // Task 3 - тут чет по условию непонятно, что нужно вернуть по тексту похоже на String.
-    // Скоре всего нужно дорабтывать и работать не со стрингой, а с интами и их сортировать,
-    // так как стринг 10 будет после единицы что не есть корректным
-//{"1, 2, 0", "4, 200, 5", "3", "3"};
+
     public static List<Integer> ReturnStringSortArray(String[] array) {
         List<String> numbersList = Arrays.asList(array);
-        String number = numbersList.stream()
-                                   .collect(Collectors.joining(", "));
-        List<Integer> numberSorted =
-                Arrays.stream(number.split(", "))
-                      .map(Integer::parseInt)
-                      .sorted(Comparator.naturalOrder())
-                      .collect(Collectors.toList());
-        return numberSorted;
+        String number = String.join(", ", numbersList);
+        return Arrays.stream(number.split(", "))
+                     .map(Integer::parseInt)
+                     .sorted(Comparator.naturalOrder())
+                     .collect(Collectors.toList());
     }
 
     //Task 4
@@ -85,22 +82,18 @@ public class HomeWorkTen {
     public static Stream<Long> LongStream(long a, long c, long m, long seed, int limit) {
         return Stream.iterate(seed, n -> (a * n + c) % m)
                      .limit(limit);
-
-
     }
 
     //Task 5
-
     public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
-        return Stream
-                .concat(first, second)
-                .collect(Collectors.collectingAndThen(
-                        Collectors.toList(),
-                        collected -> {
-                            Collections.shuffle(collected);
-                            return collected.stream();
-                        }
-                ));
+        Iterator<T> firstIterate = first.iterator();
+        Iterator<T> secondIterate = second.iterator();
+        Stream<T> result = Stream.empty();
+        while (firstIterate.hasNext() & secondIterate.hasNext()) {
+            result = Stream.concat(result, Stream.of(firstIterate.next(),
+                    secondIterate.next()));
+        }
+        return result;
     }
 }
 
---
+++ src/main/java/ua/goit/modul/modul11/StartupTime.java
@@ -0,0 +1,4 @@
+package ua.goit.modul.modul11;
+
+public class StartupTime {
+}
